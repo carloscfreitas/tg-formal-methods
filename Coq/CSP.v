8614d85ea7b1e52a6ff6018a3bbd791ba592f9b3
Gustavo Carvalho <ghpc@cin.ufpe.br>
2020-03-18 13:38:25 -0300

    Adding preliminary version of SOS.

--- Coq/CSP.v
+++ Coq/CSP.v
@@ -81,3 +81,87 @@ Definition PPay := "PAY" == "cash" --> "receipt" --> SKIP
                                                        [] "reject" --> (@ "PAY")).
 Definition PPurchase := "PURCHASE" == (@ "CHOOSE") ; (@ "PAY").
 Print PPurchase.
+
+(* TODO: here, "string" is an event in the semantic level, with tick and tau *)
+
+(* TODO: I would prefer proc_body -> event -> proc_body, but this
+         change has an impact in the notation definition before *)
+
+Reserved Notation "P '//' a '==>' Q" (at level 150, left associativity).
+Inductive sosR : proc_body -> string -> proc_body -> Prop :=
+  | prefix_rule (P Q : proc_body) (a : string) :
+      (a --> P) // a ==> Q
+  where "P '//' a '==>' Q" := (sosR P a Q).
+
+Reserved Notation "P '///' t '==>' Q" (at level 150, left associativity).
+Inductive sosStarR : proc_body -> list string -> proc_body -> Prop :=
+  | sos_empty_rule (P : proc_body) :
+      P /// nil ==> P
+  | sos_transitive_rule (P R Q : proc_body) (head : string) (tail : list string) :
+      (P // head ==> R) -> (R /// tail ==> Q) -> (P /// (head :: tail) ==> Q)
+  where "P '///' t '==>' Q" := (sosStarR P t Q).
+  
+Definition traceBodyR (body : proc_body) (t : list string) :=
+  exists (body' : proc_body), body /// t ==> body'.
+  
+Definition traceR (P : proc_def) (t : list string) :=
+  match P with
+  | Proc name body => traceBodyR body t
+  end.
+
+Definition PRINTER0 := "PRINTER0" == "accept" --> "print" --> STOP.
+
+Local Open Scope string.
+Example PRINTER0_empty_trace : traceR PRINTER0 nil.
+Proof.
+  unfold traceR. simpl.
+  exists ("accept" --> "print" --> STOP).
+  apply sos_empty_rule.
+Qed.
+
+Example PRINTER0_trace1 : traceR PRINTER0 ["accept"].
+Proof.
+  unfold traceR. simpl.
+  exists ("print" --> STOP).
+  apply sos_transitive_rule with (R := "print" --> STOP).
+  - apply prefix_rule.
+  - apply sos_empty_rule.
+Qed.
+
+(* TODO: for some reason, writing ["accept" ; "print"] raises an error
+         I have the impression that it is related to your notation *)
+
+Example PRINTER0_trace2 : traceR PRINTER0 ("accept" :: ["print"]).
+Proof.
+  unfold traceR. simpl.
+  exists (STOP).
+  apply sos_transitive_rule with (R := "print" --> STOP).
+  - apply prefix_rule.
+  - apply sos_transitive_rule with (R := STOP).
+    + apply prefix_rule.
+    + apply sos_empty_rule.
+Qed.
+
+(* Note the following proof with eapply. In some sense, it's easier.
+   However, in the end we get "remaining goals are on the shelf".
+   
+   To conclude the proof, I removed this goal from the shelf,
+   and provided a trivial example of a proc_body.
+   
+   More information about "the shelf":
+   - https://github.com/coq/coq/issues/8770
+   - https://coq-club.inria.narkive.com/PQalG3f4/how-to-solve-trivial-evars-automatically-so-that-they-don-t-get-shelved
+*)
+
+Example PRINTER0_trace2' : traceR PRINTER0 ("accept" :: ["print"]).
+Proof.
+  unfold traceR. simpl.
+  exists (STOP).
+  eapply sos_transitive_rule.
+  - apply prefix_rule.
+  - eapply sos_transitive_rule.
+    + apply prefix_rule.
+    + apply sos_empty_rule.
+  Unshelve. exact SKIP.
+Qed.
+Local Close Scope string.
