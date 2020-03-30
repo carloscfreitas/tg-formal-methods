9255496ba116420ace52bc01ba3f6550f9e42510
Carlos Freitas <casc2@cin.ufpe.br>
2020-03-29 22:42:47 -0300

    Adding context (specification) to SOS and Trace definitions and rewriting the examples

--- Coq/CSP.v
+++ Coq/CSP.v
@@ -59,7 +59,8 @@ Definition event_dec := string_dec.
 (* Notations for declaring sets of events. *)
 Notation "{{ }}" := (empty_set event) (format "{{ }}").
 Notation "{{ x }}" := (set_add event_dec x (empty_set event)).
-Notation "{{ x , y , .. , z }}" := (set_add event_dec x (set_add event_dec y .. (set_add event_dec z (empty_set event)) ..)).
+Notation "{{ x , y , .. , z }}" := (set_add event_dec x (set_add event_dec y
+    .. (set_add event_dec z (empty_set event)) ..)).
 
 (* Process reference (coercion). *)
 Definition str_to_proc_body (s : string) : proc_body := ProcRef s.
@@ -103,85 +104,112 @@ Definition SPurchase :=
     ]
   ).
 
-Compute get_proc_body SPurchase "PURCHASE".
+Compute get_proc_body SPurchase "CHOOSE".
 
 (* TODO Add context (specification) to this type.
   Revisit the transition rules bellow and check their alphabet. *)
-Reserved Notation "P '//' a '==>' Q" (at level 150, left associativity).
-Inductive sosR : proc_body -> event_tau_tick -> proc_body -> Prop :=
+Reserved Notation "C '#' P '//' a '==>' Q" (at level 150, left associativity).
+Inductive sosR : specification -> proc_body -> event_tau_tick -> proc_body -> Prop :=
   (* Prefix *)
-  | prefix_rule (P Q : proc_body) (a : event) :
-      (a --> P) // Event a ==> Q
+  | prefix_rule (C : specification) (P Q : proc_body) (a : event) :
+      C # (a --> P) // Event a ==> Q
   (* Reference *)
-  | reference_rule (P : proc_body) (name : string) :
-      eq P (ProcRef name) -> P // Tau ==> P (*TODO (get_proc_body spec name) *)
+  | reference_rule (C : specification) (P : proc_body) (name : string) :
+      forall (Q : proc_body) (a : event),
+      eq P (ProcRef name) ->
+      (C # (get_proc_body C name) // Event a ==> Q) ->
+      C # P // Event a ==> Q
   (* External Choice *)
-  | ext_choice_left_rule (P Q : proc_body) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> (P [] Q // Event a ==> P')
-  | ext_choice_right_rule (P Q : proc_body) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> (Q [] P // Event a ==> P')
-  (* Internal Choice. TODO This definition is not working properly. *)
-  | int_choice_left_rule (P Q : proc_body) :
-      forall (tau : string), P |~| Q // Tau ==> P
-  | int_choice_right_rule (P Q : proc_body) (tau : string) :
-      forall (tau : string), P |~| Q // Tau ==> Q
+  | ext_choice_left_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body) (a : event), (C # P // Event a ==> P') -> (C # P [] Q // Event a ==> P')
+  | ext_choice_right_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body) (a : event), (C # P // Event a ==> P') -> (C # Q [] P // Event a ==> P')
+  (* Internal Choice. TODO Check this definition. *)
+  | int_choice_left_rule (C : specification) (P Q : proc_body) :
+      forall (R : proc_body) (a : event),
+      (C # P // Event a ==> R) -> C # P |~| Q // Event a ==> R
+  | int_choice_right_rule (C : specification) (P Q : proc_body) :
+      forall (R : proc_body) (a : event),
+      (C # P // Event a ==> R) ->  C # Q |~| P // Event a ==> R
   (* Alphabetised Parallel *)
-  | alpha_parall_indep_left_rule (P Q : proc_body) (A B : alphabet) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> (ProcAlphaParallel P Q A B) // Event a ==> (ProcAlphaParallel P' Q A B)
-  | alpha_parall_indep_right_rule (P Q : proc_body) (A B : alphabet) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> (ProcAlphaParallel Q P A B) // Event a ==> (ProcAlphaParallel Q P' A B)
-  | alpha_parall_joint_rule (P Q : proc_body) (A B : alphabet) :
-      forall (P' Q' : proc_body) (a : event), (P // Event a ==> P') -> (Q // Event a ==> Q') -> (ProcAlphaParallel P Q A B) // Event a ==> (ProcAlphaParallel P' Q' A B)
+  | alpha_parall_indep_left_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (P' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      C # ProcAlphaParallel P Q A B // Event a ==> ProcAlphaParallel P' Q A B
+  | alpha_parall_indep_right_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (P' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      C # ProcAlphaParallel Q P A B // Event a ==> ProcAlphaParallel Q P' A B
+  | alpha_parall_joint_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (P' Q' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      (C # Q // Event a ==> Q') ->
+      C # ProcAlphaParallel P Q A B // Event a ==> ProcAlphaParallel P' Q' A B
   (* Generalised Parallel *)
-  | gener_parall_indep_left_rule (P Q : proc_body) (A : alphabet) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> P [| A |] Q // Event a ==> P' [| A |] Q
-  | gener_parall_indep_right_rule (P Q : proc_body) (A : alphabet) :
-    forall (P' : proc_body) (a : event), (P // Event a ==> P') -> Q [| A |] P // Event a ==> Q [| A |] P'
-  | gener_parall_joint_rule (P Q : proc_body) (A : alphabet) :
-      forall (P' Q' : proc_body) (a : event), (P // Event a ==> P') -> (Q // Event a ==> Q') -> P [| A |] Q // Event a ==> P' [| A |] Q'
+  | gener_parall_indep_left_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (P' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      C # P [| A |] Q // Event a ==> P' [| A |] Q
+  | gener_parall_indep_right_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (P' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      C # Q [| A |] P // Event a ==> Q [| A |] P'
+  | gener_parall_joint_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (P' Q' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      (C # Q // Event a ==> Q') ->
+      C # P [| A |] Q // Event a ==> P' [| A |] Q'
   (* Interleave *)
   | interleave_left_rule (P Q : proc_body) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> P ||| Q // Event a ==> P' ||| Q
-  | interleave_right_rule (P Q : proc_body) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> Q ||| P // Event a ==> Q ||| P'
-  | interleave_tick_rule (P Q : proc_body) :
-      (* TODO Check if there is indeed a conjuction in this transition rule. *)
-      forall (P' Q' : proc_body), (P // Tick ==> P') /\ (Q // Tick ==> Q') -> P ||| Q // Tick ==> P' ||| Q'
+      forall (C : specification) (P' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      C # P ||| Q // Event a ==> P' ||| Q
+  | interleave_right_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body) (a : event),
+      (C # P // Event a ==> P') ->
+      C # Q ||| P // Event a ==> Q ||| P'
+  | interleave_tick_rule (C : specification) (P Q : proc_body) : (* TODO Check if there is indeed a conjuction in this transition rule. *)
+      forall (P' Q' : proc_body),
+      (C # P // Tick ==> P') /\ (C # Q // Tick ==> Q') ->
+      C # P ||| Q // Tick ==> P' ||| Q'
   (* Sequential Composition *)
-  | seq_comp_rule (P Q : proc_body) :
-      forall (P' : proc_body) (a : event), (P // Event a ==> P') -> P ;; Q // Event a ==> P' ;; Q
-  | seq_comp_tick_rule (P Q : proc_body) :
-      forall (P' : proc_body), (P // Tick ==> P') -> P ;; Q // Tau ==> Q
-  where "P '//' a '==>' Q" := (sosR P a Q).
+  | seq_comp_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body) (a : event),
+      (C # P // Event a ==> P') -> C # P ;; Q // Event a ==> P' ;; Q
+  | seq_comp_tick_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body), (C # P // Tick ==> P') -> C # P ;; Q // Tau ==> Q
+  where "C '#' P '//' a '==>' Q" := (sosR C P a Q).
 
-Reserved Notation "P '///' t '==>' Q" (at level 150, left associativity).
-Inductive sosStarR : proc_body -> list event_tau_tick -> proc_body -> Prop :=
-  | sos_empty_rule (P : proc_body) :
-      P /// nil ==> P
-  | sos_transitive_rule (P R Q : proc_body) (head : event_tau_tick) (tail : list event_tau_tick) :
-      (P // head ==> R) -> (R /// tail ==> Q) -> (P /// (head :: tail) ==> Q)
-  where "P '///' t '==>' Q" := (sosStarR P t Q).
+Reserved Notation "C '#' P '///' t '==>' Q" (at level 150, left associativity).
+Inductive sosStarR : specification -> proc_body -> list event_tau_tick -> proc_body -> Prop :=
+  | sos_empty_rule (C : specification) (P : proc_body) :
+      C # P /// nil ==> P
+  | sos_transitive_rule (C : specification) (P R Q : proc_body) (head : event_tau_tick) (tail : list event_tau_tick) :
+      (C # P // head ==> R) -> (C # R /// tail ==> Q) -> (C # P /// (head :: tail) ==> Q)
+  where "C '#' P '///' t '==>' Q" := (sosStarR C P t Q).
   
-Definition traceBodyR (body : proc_body) (t : list event_tau_tick) :=
-  exists (body' : proc_body), body /// t ==> body'.
+Definition traceBodyR (C : specification) (body : proc_body) (t : list event_tau_tick) :=
+  exists (body' : proc_body), C # body /// t ==> body'.
   
-Definition traceR (P : proc_def) (t : list event_tau_tick) :=
+Definition traceR (C : specification) (P : proc_def) (t : list event_tau_tick) :=
   match P with
-  | Proc name body => traceBodyR body t
+  | Proc name body => traceBodyR C body t
   end.
 
 (** TRACE EXAMPLES **)
 
+Definition CH_PRINTER0 := Channel {{"accept", "print"}}.
 Definition PRINTER0 := "PRINTER0" ::= "accept" --> "print" --> STOP.
+Definition S_PRINTER0 := Spec [CH_PRINTER0] [PRINTER0].
 
-Example PRINTER0_empty_trace : traceR PRINTER0 nil.
+Example PRINTER0_empty_trace : traceR S_PRINTER0 PRINTER0 nil.
 Proof.
   unfold traceR. simpl.
   exists ("accept" --> "print" --> STOP).
   apply sos_empty_rule.
 Qed.
 
-Example PRINTER0_trace1 : traceR PRINTER0 [Event "accept"].
+Example PRINTER0_trace1 : traceR S_PRINTER0 PRINTER0 [Event "accept"].
 Proof.
   unfold traceR. simpl.
   exists ("print" --> STOP).
@@ -190,7 +218,7 @@ Proof.
   - apply sos_empty_rule.
 Qed.
 
-Example PRINTER0_trace2 : traceR PRINTER0 [Event "accept" ; Event "print"].
+Example PRINTER0_trace2 : traceR S_PRINTER0 PRINTER0 [Event "accept" ; Event "print"].
 Proof.
   unfold traceR. simpl.
   exists (STOP).
@@ -212,7 +240,7 @@ Qed.
    - https://coq-club.inria.narkive.com/PQalG3f4/how-to-solve-trivial-evars-automatically-so-that-they-don-t-get-shelved
 *)
 
-Example PRINTER0_trace2' : traceR PRINTER0 [Event "accept" ; Event "print"].
+Example PRINTER0_trace2' : traceR S_PRINTER0 PRINTER0 [Event "accept" ; Event "print"].
 Proof.
   unfold traceR. simpl.
   exists (STOP).
@@ -224,9 +252,11 @@ Proof.
   Unshelve. exact SKIP.
 Qed.
 
-Definition CHOOSE := "CHOOSE" ::= "select" --> ("keep" --> SKIP 
+Definition CH_CHOOSE := Channel {{"select", "keep", "return"}}.
+Definition P_CHOOSE := "CHOOSE" ::= "select" --> ("keep" --> SKIP
                                                 [] "return" --> "CHOOSE").
-Example CHOOSE_trace1 : traceR CHOOSE [Event "select" ; Event "keep"].
+Definition S_CHOOSE := Spec [CH_CHOOSE] [P_CHOOSE].
+Example CHOOSE_trace1 : traceR S_CHOOSE P_CHOOSE [Event "select" ; Event "keep"].
 Proof.
   unfold traceR. simpl.
   exists (SKIP).
@@ -238,7 +268,7 @@ Proof.
     * apply sos_empty_rule.
 Qed.
 
-Example CHOOSE_trace2 : traceR CHOOSE [Event "select" ; Event "return"].
+Example CHOOSE_trace2 : traceR S_CHOOSE P_CHOOSE [Event "select" ; Event "return"].
 Proof.
   unfold traceR. simpl.
   exists ("CHOOSE").
@@ -250,6 +280,7 @@ Proof.
     * apply sos_empty_rule.
 Qed.
 
+Definition CH_TEAM := Channel {{"lift_piano", "lift_table"}}.
 Definition PETE := "PETE" ::= "lift_piano" --> "PETE"
                               |~| "lift_table" --> "PETE".
 
@@ -258,15 +289,27 @@ Definition DAVE := "DAVE" ::= "lift_piano" --> "DAVE"
 
 Definition TEAM := "TEAM" ::= "PETE" [| Alphabet {{"lift_piano", "lift_table"}} |] "DAVE".
 
-(* TODO Revisit once the reference_rule is defined. *)
-Example TEAM_trace1 : traceR TEAM [Event "lift_piano"].
+Definition S_TEAM := Spec [CH_TEAM] [PETE ; DAVE ; TEAM].
+
+Example TEAM_trace1 : traceR S_TEAM TEAM [Event "lift_piano"].
 Proof.
   unfold traceR. simpl.
-  exists ("TEAM").
+  exists ("PETE" [| Alphabet {{"lift_piano", "lift_table"}} |] "DAVE").
   apply sos_transitive_rule with (R := "PETE" [| Alphabet {{"lift_piano", "lift_table"}} |] "DAVE").
-  - apply gener_parall_joint_rule. Abort.
+  - apply gener_parall_joint_rule.
+    * apply reference_rule with (name := "PETE").
+      + reflexivity.
+      + simpl. apply int_choice_left_rule. apply prefix_rule.
+    * apply reference_rule with (name := "DAVE").
+      + reflexivity.
+      + simpl. apply int_choice_left_rule. apply prefix_rule.
+  - apply sos_empty_rule.
+Qed.
 
-Example LIGHT_trace1 : traceR ("LIGHT" ::= "on" --> "off" --> "LIGHT") [Event "on" ; Event "off" ; Event "on"].
+Definition LIGHT := "LIGHT" ::= "on" --> "off" --> "LIGHT".
+Definition S_LIGHT := Spec [Channel {{"on", "off"}}] [LIGHT].
+
+Example LIGHT_trace1 : traceR S_LIGHT LIGHT [Event "on" ; Event "off" ; Event "on"].
 Proof.
   unfold traceR. simpl.
   exists ("off" --> "LIGHT").
@@ -279,30 +322,51 @@ Proof.
       + apply sos_empty_rule.
 Qed.
 
-Definition PUMP1 := "lift_nozzle_1" --> "READY1".
-Definition READY1 := "replace_nozzle_1" --> "PUMP1"
-                      [] "depress_trigger_1" --> "release_trigger_1" --> "READY1".
-Definition PUMP2 := "lift_nozzle_2" --> "READY2".
-Definition READY2 := "replace_nozzle_2" --> "PUMP2"
-                      [] "depress_trigger_2" --> "release_trigger_2" --> "READY2".
-Definition FORECOURT := "FORECOURT" ::= PUMP1 ||| PUMP2.
+Definition FORECOURT := "FORECOURT" ::= "PUMP1" ||| "PUMP2".
+Definition S_FORECOURT :=
+  (
+    Spec
+    [
+      Channel {{"lift_nozzle_1", "replace_nozzle_1", "depress_trigger_1", "release_trigger_1"}}
+      ; Channel {{"lift_nozzle_2", "replace_nozzle_2", "depress_trigger_2", "release_trigger_2"}}
+    ]
+    [
+      "PUMP1" ::= "lift_nozzle_1" --> "READY1"
+      ; "READY1" ::= "replace_nozzle_1" --> "PUMP1"
+                      [] "depress_trigger_1" --> "release_trigger_1" --> "READY1"
+      ; "PUMP2" ::= "lift_nozzle_2" --> "READY2"
+      ; "READY2" ::= "replace_nozzle_2" --> "PUMP2"
+                      [] "depress_trigger_2" --> "release_trigger_2" --> "READY2"
+      ; "FORECOURT" ::= "PUMP1" ||| "PUMP2"
+    ]
+  ).
 
-(* TODO Revisit once the reference_rule is defined. *)
-Example FORECOURT_trace1 : traceR FORECOURT [Event "lift_nozzle_1" ; Event "depress_trigger_1" ; Event "lift_nozzle_2" ;
-                                            Event "depress_trigger_2" ; Event "release_trigger_2"].
+Example FORECOURT_trace1 : traceR S_FORECOURT ("FORECOURT" ::= "PUMP1" ||| "PUMP2")
+    [Event "lift_nozzle_1" ; Event "depress_trigger_1" ; Event "lift_nozzle_2"
+    ; Event "depress_trigger_2" ; Event "release_trigger_2"].
 Proof.
   unfold traceR. simpl.
-  exists("release_trigger_1" --> READY1 ||| READY2).
-  apply sos_transitive_rule with (R := READY1 ||| PUMP2).
-  - apply interleave_left_rule. unfold PUMP1. apply prefix_rule.
-  - apply sos_transitive_rule with (R := "release_trigger_1" --> READY1 ||| PUMP2).
-    * apply interleave_left_rule. apply ext_choice_right_rule. apply prefix_rule.
-    * apply sos_transitive_rule with (R := "release_trigger_1" --> READY1 ||| READY2).
-      + apply interleave_right_rule. unfold PUMP2. apply prefix_rule.
-      + apply sos_transitive_rule with (R := "release_trigger_1" --> READY1 ||| "release_trigger_2" --> READY2).
-        { apply interleave_right_rule. apply ext_choice_right_rule. apply prefix_rule. }
+  exists("release_trigger_1" --> "READY1" ||| "READY2").
+  apply sos_transitive_rule with (R := "READY1" ||| "PUMP2").
+  - apply interleave_left_rule. apply reference_rule with (name := "PUMP1").
+    * reflexivity.
+    * simpl. apply prefix_rule.
+  - apply sos_transitive_rule with (R := "release_trigger_1" --> "READY1" ||| "PUMP2").
+    * apply interleave_left_rule. apply reference_rule with (name := "READY1").
+      + reflexivity.
+      + simpl. apply ext_choice_right_rule. apply prefix_rule.
+    * apply sos_transitive_rule with (R := "release_trigger_1" --> "READY1" ||| "READY2").
+      + apply interleave_right_rule. apply reference_rule with (name := "PUMP2").
+          { reflexivity. }
+          { simpl. apply prefix_rule. }
+      + apply sos_transitive_rule with (R := "release_trigger_1" --> "READY1" ||| "release_trigger_2" --> "READY2").
         { 
-          apply sos_transitive_rule with (R := "release_trigger_1" --> READY1 ||| READY2).
+          apply interleave_right_rule. apply reference_rule with (name := "READY2").
+          { reflexivity. }
+          { simpl. apply ext_choice_right_rule. apply prefix_rule. }
+        }
+        { 
+          apply sos_transitive_rule with (R := "release_trigger_1" --> "READY1" ||| "READY2").
           { apply interleave_right_rule. apply prefix_rule. }
           { apply sos_empty_rule. }
         }
