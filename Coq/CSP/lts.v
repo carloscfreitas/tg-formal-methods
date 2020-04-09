307fa6f7eddafc9aac582793e3e638da3318c1ad
Gustavo Carvalho <ghpc@cin.ufpe.br>
2020-04-09 01:21:03 -0300

    Updating repository structure, correcting errors in the sos_semantics, and definitin ltsR'

---
+++ Coq/CSP/lts.v
@@ -0,0 +1,186 @@
+Require Import Lists.List.
+Require Import Lists.ListSet.
+Require Import Strings.String.
+Import ListNotations.
+
+Require Import syntax.
+Require Import semantics_sos.
+
+(** LTS RELATION **)
+
+Inductive transitionR : specification -> (proc_body * event_tau_tick * proc_body) -> proc_body -> Prop :=
+  | transition_rule (C : specification) (P' : proc_body) (a : event_tau_tick) (Q : proc_body)
+    (P : proc_body)  (Tc : list event_tau_tick) :
+      (C # P /// Tc ==> P') ->
+      (C # P' // a ==> Q) ->
+      transitionR C (P', a, Q) P.
+
+Local Open Scope string.
+
+Definition TOY_PROBLEM := Spec [Channel {{"a", "b"}}] ["P" ::= "a" --> "b" --> STOP].
+
+Example test1 : transitionR TOY_PROBLEM ("a" --> "b" --> STOP, Event "a", "b" --> STOP) ("a" --> "b" --> STOP).
+Proof.
+  apply transition_rule with (Tc := nil).
+  - apply sos_empty_rule.
+  - apply prefix_rule.
+Qed.
+
+Example test2 : transitionR TOY_PROBLEM ("b" --> STOP, Event "b", STOP) ("a" --> "b" --> STOP).
+Proof.
+  apply transition_rule with (Tc := [Event "a"]).
+  - apply sos_transitive_rule with (R := "b" --> STOP).
+    * apply prefix_rule.
+    * apply sos_empty_rule.
+  - apply prefix_rule.
+Qed.
+
+Inductive transitionsR : specification -> list (proc_body * event_tau_tick * proc_body) -> proc_body -> Prop :=
+  | transitions_empty_rule (C : specification) (P : proc_body) :
+      transitionsR C nil P
+  | transitions_rule (C : specification) (P : proc_body) (head : proc_body * event_tau_tick * proc_body)
+    (tail : list (proc_body * event_tau_tick * proc_body)) :
+      transitionR C head P ->
+      transitionsR C tail P ->
+      transitionsR C (head :: tail) P.
+
+Example test3 :
+  transitionsR
+    TOY_PROBLEM 
+    [
+      ("b" --> STOP, Event "b", STOP)
+      ; ("a" --> "b" --> STOP, Event "a", "b" --> STOP)
+    ] 
+    ("a" --> "b" --> STOP).
+Proof.
+  apply transitions_rule.
+  - apply transition_rule with (Tc := [Event "a"]).
+    * apply sos_transitive_rule with (R := "b" --> STOP).
+      + apply prefix_rule.
+      + apply sos_empty_rule.
+    * apply prefix_rule.
+  - apply transitions_rule.
+    * apply transition_rule with (Tc := nil).
+      + apply sos_empty_rule.
+      + apply prefix_rule.
+    * apply transitions_empty_rule.
+Qed.
+
+Inductive ltsR (C : specification) (T : list (proc_body * event_tau_tick * proc_body)) (P : proc_body) : Prop :=
+  | lts_rule :
+      (forall (t : proc_body * event_tau_tick * proc_body), transitionR C t P -> In t T) ->
+      transitionsR C T P ->
+      ltsR C T P.
+
+(* TODO Complete this proof. *)
+Example lts1 :
+  ltsR TOY_PROBLEM [("a" --> STOP, Event "a", STOP)] ("a" --> STOP).
+Proof.
+    apply lts_rule.
+    - intros. destruct H. simpl. left. Abort.
+
+Theorem event_tau_tick_eq_dec :
+  forall (e1 e2 : event_tau_tick),
+    {e1 = e2} + {e1 <> e2}.
+Proof.
+  intros. destruct e1, e2 ; try (left ; reflexivity) ;
+  try (right ; unfold not ; intros H ; now inversion H).
+  decide equality ; apply event_dec.
+Qed.
+
+Theorem alphabet_eq_dec :
+  forall (a1 a2 : alphabet),
+    {a1 = a2} + {a1 <> a2}.
+Proof.
+  intros. destruct a1, a2.
+  repeat (decide equality ; try apply event_dec).
+Qed.
+
+Theorem proc_body_eq_dec :
+  forall (p1 p2 : proc_body),
+    {p1 = p2} + {p1 <> p2}.
+Proof.
+  intros. destruct p1, p2 ;
+    try (right ; unfold not ; intros H ; now inversion H) ;
+    try (left ; reflexivity) ;
+    decide equality ; try apply event_dec ; try apply alphabet_eq_dec.
+Qed.
+
+Theorem transition_eq_dec :
+  forall (t1 t2 : (proc_body * event_tau_tick * proc_body)),
+    {t1 = t2} + {t1 <> t2}.
+Proof.
+  intros. decide equality.
+  - decide equality ; try apply event_dec ; try apply alphabet_eq_dec.
+  - decide equality ; try apply event_tau_tick_eq_dec ; try apply proc_body_eq_dec.
+Qed.
+
+Fixpoint get_target_states (T : set (proc_body * event_tau_tick * proc_body)) : (set proc_body) :=
+  match T with
+  | nil              => nil
+  | (p,a,p') :: tail => set_add proc_body_eq_dec p' (get_target_states tail)
+  end.
+
+Inductive ltsR' : specification -> set (proc_body * event_tau_tick * proc_body) -> set proc_body -> Prop :=
+  | lts_empty_rule (C : specification) (S : set proc_body) (P : proc_body) :
+    Datatypes.length S = 1 ->
+    In P S ->
+    ~ (exists (a : event_tau_tick) (P' : proc_body), C # P // a ==> P') ->
+    ltsR' C nil S
+  | lts_inductive_rule (C : specification) (T T' T'' : set (proc_body * event_tau_tick * proc_body)) (P : proc_body) (L L' : set proc_body) :
+    In P L ->
+    (forall (a : event_tau_tick) (P' : proc_body), (C # P // a ==> P') <-> In (P,a,P') T') ->
+    L' = get_target_states T' ->
+    ltsR' C T'' L' ->
+    T = (set_union transition_eq_dec T' T'') ->
+    ltsR' C T L.
+
+Example lts1' :
+  ltsR'
+    (* context *)
+    TOY_PROBLEM (* context *)
+    (* LTS *)
+    [ ("a" --> "b" --> STOP, Event "a", "b" --> STOP) ;
+      ("b" --> STOP, Event "b", STOP) ]
+    (* INITIAL STATE *)
+    ["a" --> "b" --> STOP].
+Proof.
+  eapply lts_inductive_rule with
+    (T' := [("a" --> "b" --> STOP, Event "a", "b" --> STOP)])
+    (L' := ["b" --> STOP]).
+  - simpl. left. reflexivity.
+  - intros. split.
+    + intros. inversion H. subst.
+      * simpl. left. reflexivity.
+      * subst. inversion H0.
+    + intros. simpl in H. destruct H.
+      * inversion H. subst. apply prefix_rule.
+      * inversion H.
+  - intros. simpl. reflexivity.
+  - eapply lts_inductive_rule with
+    (T' := [("b" --> STOP, Event "b", STOP)])
+    (L' := [STOP]).
+    + simpl. left. reflexivity.
+    + intros. split.
+      * intros. inversion H. subst.
+        { simpl. left. reflexivity. }
+        { subst. inversion H0. }
+      * intros. simpl in H. destruct H.
+        { inversion H. subst. apply prefix_rule. }
+        { inversion H. }
+    + simpl. reflexivity.
+    + eapply lts_empty_rule.
+      * simpl. reflexivity.
+      * simpl. left. reflexivity.
+      * unfold not. intros. destruct H. destruct H.
+        inversion H. subst. inversion H0.
+    + simpl. reflexivity.
+  - simpl.
+    destruct (transition_eq_dec ("b" --> STOP, Event "b", STOP)
+                                ("a" --> "b" --> STOP, Event "a", "b" --> STOP))
+             eqn:H.
+    + inversion e.
+    + reflexivity.
+Qed.
+
+Local Close Scope string.
---
+++ Coq/CSP/semantics_sos.v
@@ -0,0 +1,143 @@
+Require Import Lists.List.
+Require Import Lists.ListSet.
+Require Import Strings.String.
+Import ListNotations.
+
+Require Import syntax.
+
+Reserved Notation "C '#' P '//' a '==>' Q" (at level 150, left associativity).
+Inductive sosR : specification -> proc_body -> event_tau_tick -> proc_body -> Prop :=
+  (* Prefix *)
+  | prefix_rule (C : specification) (P : proc_body) (a : event) :
+      C # (a --> P) // Event a ==> P
+  (* Reference *)
+  (* This inference rule is different from the one implemented in the FDR tool. This approach will
+    allow an ill-formed recursion such as P = P to be defined, by introducing a transition which communicates
+    the internal event tau and, therefore, increasing the process LTS size. *)
+  | reference_rule (C : specification) (P : proc_body) (name : string) :
+      forall (Q : proc_body),
+        eq P (ProcRef name) ->
+        eq (get_proc_body C name) Q ->
+        C # P // Tau ==> Q
+  (* External Choice *)
+  | ext_choice_left_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body) (a : event_tau_tick),
+        ~ eq a Tau ->
+        (C # P // a ==> P') ->
+        (C # P [] Q // a ==> P')
+  | ext_choice_right_rule (C : specification) (P Q : proc_body) :
+      forall (Q' : proc_body) (a : event_tau_tick),
+        ~ eq a Tau ->
+        (C # Q // a ==> Q') ->
+        (C # P [] Q // a ==> Q')
+  | ext_choice_tau_left_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body),
+        (C # P // Tau ==> P') ->
+        (C # P [] Q // Tau ==> P' [] Q)
+  | ext_choice_tau_right_rule (C : specification) (P Q : proc_body) :
+      forall (Q' : proc_body),
+        (C # Q // Tau ==> Q') ->
+        (C # P [] Q // Tau ==> P [] Q')
+  (* Internal Choice *)
+  | int_choice_left_rule (C : specification) (P Q : proc_body) :
+      C # P |~| Q // Tau ==> P
+  | int_choice_right_rule (C : specification) (P Q : proc_body) :
+      C # P |~| Q // Tau ==> Q
+  (* Alphabetised Parallel *)
+  | alpha_parall_indep_left_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (P' : proc_body) (a : event),
+        set_In a (set_diff event_dec A B) ->
+        (C # P // Event a ==> P') ->
+        C # P [[ A \\ B ]] Q // Event a ==> P' [[ A \\ B ]] Q
+  | alpha_parall_indep_right_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (Q' : proc_body) (a : event),
+        set_In a (set_diff event_dec B A) ->
+        (C # Q // Event a ==> Q') ->
+        C # P [[ A \\ B ]] Q // Event a ==> P [[ A \\ B ]] Q'
+  | alpha_parall_tau_indep_left_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (P' : proc_body),
+        (C # P // Tau ==> P') ->
+        C # P [[ A \\ B ]] Q // Tau ==> P' [[ A \\ B ]] Q
+  | alpha_parall_tau_indep_right_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (Q' : proc_body),
+        (C # Q // Tau ==> Q') ->
+        C # P [[ A \\ B ]] Q // Tau ==> P [[ A \\ B ]] Q'
+  | alpha_parall_joint_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (P' Q' : proc_body) (a : event),
+        set_In a (set_inter event_dec A B) ->
+        (C # P // Event a ==> P') ->
+        (C # Q // Event a ==> Q') ->
+        C # P [[ A \\ B ]] Q // Event a ==> P' [[ A \\ B ]] Q'
+  | alpha_parall_tick_joint_rule (C : specification) (P Q : proc_body) (A B : alphabet) :
+      forall (P' Q' : proc_body),
+        (C # P // Tick ==> P') ->
+        (C # Q // Tick ==> Q') ->
+        C # P [[ A \\ B ]] Q // Tick ==> P' [[ A \\ B ]] Q'
+  (* Generalised Parallel *)
+  | gener_parall_indep_left_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (P' : proc_body) (a : event),
+        ~ set_In a A ->
+        (C # P // Event a ==> P') ->
+        C # P [| A |] Q // Event a ==> P' [| A |] Q
+  | gener_parall_indep_right_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (Q' : proc_body) (a : event),
+        ~ set_In a A ->
+        (C # Q // Event a ==> Q') ->
+        C # P [| A |] Q // Event a ==> P [| A |] Q'
+  | gener_parall_tau_indep_left_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (P' : proc_body),
+        (C # P // Tau ==> P') ->
+        C # P [| A |] Q // Tau ==> P' [| A |] Q
+  | gener_parall_tau_indep_right_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (Q' : proc_body),
+        (C # Q // Tau ==> Q') ->
+        C # P [| A |] Q // Tau ==> P [| A |] Q'
+  | gener_parall_joint_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (P' Q' : proc_body) (a : event),
+        set_In a A ->
+        (C # P // Event a ==> P') ->
+        (C # Q // Event a ==> Q') ->
+        C # P [| A |] Q // Event a ==> P' [| A |] Q'
+  | gener_parall_tick_joint_rule (C : specification) (P Q : proc_body) (A : alphabet) :
+      forall (P' Q' : proc_body),
+        (C # P // Tick ==> P') ->
+        (C # Q // Tick ==> Q') ->
+        C # P [| A |] Q // Tick ==> P' [| A |] Q'
+  (* Interleave *)
+  | interleave_left_rule (P Q : proc_body) :
+      forall (C : specification) (P' : proc_body) (a : event_tau_tick),
+        ~ eq a Tick ->
+        (C # P // a ==> P') ->
+        C # P ||| Q // a ==> P' ||| Q
+  | interleave_right_rule (C : specification) (P Q : proc_body) :
+      forall (Q' : proc_body) (a : event_tau_tick),
+        ~ eq a Tick ->
+        (C # Q // a ==> Q') ->
+        C # P ||| Q // a ==> P ||| Q'
+  | interleave_tick_rule (C : specification) (P Q : proc_body) :
+      forall (P' Q' : proc_body),
+        (C # P // Tick ==> P') ->
+        (C # Q // Tick ==> Q') ->
+        C # P ||| Q // Tick ==> P' ||| Q'
+  (* Sequential Composition *)
+  (* This inference rule is different from the one implemented in the FDR tool. The main consequence
+    of this approach is that the transition resulting from the communication of the internal event tau
+    will not be supressed and, therefore, will appear in the LTS. *)
+  | seq_comp_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body) (a : event_tau_tick),
+        ~ eq a Tick ->
+        (C # P // a ==> P') ->
+        C # P ;; Q // a ==> P' ;; Q
+  | seq_comp_tick_rule (C : specification) (P Q : proc_body) :
+      forall (P' : proc_body),
+        (C # P // Tick ==> P') ->
+        C # P ;; Q // Tau ==> Q
+  where "C '#' P '//' a '==>' Q" := (sosR C P a Q).
+
+Reserved Notation "C '#' P '///' t '==>' Q" (at level 150, left associativity).
+Inductive sosStarR : specification -> proc_body -> list event_tau_tick -> proc_body -> Prop :=
+  | sos_empty_rule (C : specification) (P : proc_body) :
+      C # P /// nil ==> P
+  | sos_transitive_rule (C : specification) (P R Q : proc_body) (head : event_tau_tick) (tail : list event_tau_tick) :
+      (C # P // head ==> R) -> (C # R /// tail ==> Q) -> (C # P /// (head :: tail) ==> Q)
+  where "C '#' P '///' t '==>' Q" := (sosStarR C P t Q).
