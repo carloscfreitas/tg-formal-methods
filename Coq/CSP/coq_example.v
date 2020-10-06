fabb0b3e0710f9c966e93a1cec0bd4529364073e
Carlos Freitas <casc2@cin.ufpe.br>
2020-10-06 19:51:16 -0300

    Adding QuickChick example

--- Coq/CSP/coq_example.v
+++ Coq/CSP/coq_example.v
@@ -10,6 +10,8 @@ Module NatPlaygroud.
     end.
 End NatPlaygroud.
 
+Require Import Coq.Init.Nat.
+
 Fixpoint plus (n : nat) (m : nat) : nat :=
   match n with
     | O => m
@@ -66,3 +68,36 @@ Ltac solve_not_in := unfold not;
 
 Example elem_not_in_list' : ~ (In 4 [0 ; 1 ; 2 ; 3]).
 Proof. solve_not_in. Qed.
+
+Lemma negb_involutive : forall (b : bool),
+  negb (negb b) = b.
+Proof.
+  destruct b.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Theorem evenb_S : forall n : nat,
+  evenb (S n) = negb (evenb n).
+Proof.
+  intros. induction n.
+  - simpl. reflexivity.
+  - simpl. simpl in IHn. rewrite IHn.
+    rewrite negb_involutive. reflexivity.
+Qed.
+
+Set Warnings "-extraction-opaque-accessed,-extraction".
+Set Warnings "-notation-overridden,-parsing".
+
+From QuickChick Require Import QuickChick.
+Require Import List ZArith. Import ListNotations.
+
+Fixpoint remove (x : nat) (l : list nat) : list nat :=
+  match l with
+    | []   => []
+    | h::t => if h =? x then t else h :: remove x t
+  end.
+
+Conjecture removeP : forall x l,  ~ (In x (remove x l)).
+
+QuickChick removeP.
